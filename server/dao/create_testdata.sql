3d0e41b2006d5f6f65e00cf9582718c2df1d1738
Mari Teiler-Johnsen <mari.teiler.johnsen@gmail.com>
2018-11-19 22:33:54 +0100

    fixing last CI test

--- server/dao/create_testdata.sql
+++ server/dao/create_testdata.sql
@@ -9,4 +9,5 @@ VALUES (DEFAULT, 'Test-Headline', 'Sport', 'Testtesttestttest', 'bildelink', 1, 
        (DEFAULT, 'Test-Headline3', 'Kultur', 'Testtesttestttest3', 'bildelink3', 1, default),
        (DEFAULT, 'Test-Headline4', 'Kultur', 'Testtesttestttest4', 'bildelink4', 1, default),
        (DEFAULT, 'Test-Headline5', 'Kultur', 'Testtesttestttest5', 'bildelink5', 1, default),
+       (DEFAULT, 'Test-Headline6', 'Kultur', 'Testtesttestttest6', 'bildelink6', 1, default),
        (DEFAULT, 'Test-Headline6', 'Kultur', 'Testtesttestttest6', 'bildelink6', 1, default);
